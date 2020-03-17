353816cf7faac831fa144a718956d28d4be0aa6f
yboodhan <yboodhan@gmail.com>
2020-03-17 12:05:08 -0700

    created list and detail functional components for products json response

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,17 @@
+# from django.urls import path
+
+# from .views import ProductDetailView, ProductListView
+
+# urlpatterns = [
+#     path("", ProductListView.as_view(), name="product-list"),
+#     path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+# ]
+
 from django.urls import path
 
-from .views import ProductDetailView, ProductListView
+from .views import product_list, product_detail
 
 urlpatterns = [
-    path("", ProductListView.as_view(), name="product-list"),
-    path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+    path("products/", product_list, name="product-list"),
+    path("products/<int:pk>/", product_detail, name="product-detail")
 ]
